526ff1b79bd4cd42199b1b950a51aff843afa2f2
LittleStarOnGH <121687115+LittleStarOnGH@users.noreply.github.com>
2026-07-29 23:26:31 +0100

    Add files via upload

---
+++ js/lang.js
@@ -0,0 +1,167 @@
+const change_lang = {
+    en: {
+        top_title: `UEA "Hologram" Project`, 
+        title: `"Hologram" Project`,
+        faculty: "SCHOOL OF ENGINEERING MATHEMATICS & PHYSICS",
+        select_below: "PLEASE SELECT BELOW",
+        btn_display: "Display",
+        btn_guest: "Guest Access",
+        btn_connect: "Connect",
+        btn_proj_info: "Project Info",
+        box_code: "Enter 4-digit code",
+        guest_remote_code: "GUEST REMOTE CODE:",
+        guest_control: "Guest Control",
+        cat_3d_objects: "3D MODELS",
+        cat_knots: "KNOTS & CURVES",
+        cat_geometry: "POLYHEDRONS & STANDARD GEOMETRY",
+        cat_complex:"COMPLEX SHAPES",
+        cat_advanced:"ADVANCED SIMULATION",
+        adjustments: "ADJUSTMENTS",
+        adj_size: "Adjusting the size",
+        adj_side_x: "Side Objects (x-axis)",
+        adj_side_y: "Side Objects (y-axis)",
+        adj_mid_y: "Middle Object (y-axis)",
+        swipe_lr: "Swipe to rotate left/right",
+        swipe_obj: "Swipe to rotate the object",
+        toggles: "TOGGLES & LIVE CAMERA",
+        btn_color: "Change Colour",
+        color_title: "Colour",
+        btn_close_apply: "Close / Apply",
+        btn_toggle_code: "Toggle Code Box",
+        btn_toggle_lines: "Toggle Cut Lines",
+        btn_toggle_lang: "Toggle Language",
+        btn_live_cam: "Live Camera",
+        supervisor: "SUPERVISOR",
+        student_intern: "STUDENT INTERN",
+        go_back: "Go Back",
+        report: "Report",
+        booklet: "Booklet",
+        flowchart: "Flowchart",
+        special_thanks: "Special Thanks:",
+        project_overview: "PROJECT OVERVIEW",
+        libraries_used: "LIBRARIES USED",
+        credits: "CREDITS",
+        dyn_temperature: "Temperature",
+        dyn_wave: "Wave Amplitude",
+        dyn_evolution: "Evolution Time",
+        eq_gp: "Gross-Pitaevskii equation:",
+        eq_holographic: "Holographic matter-gauge-gravity Lagrangian:",
+        eq_navier: "Navier-Stokes equation:",
+        eq_water_waves: "Linear water waves beneath a thin elastic ice sheet:",
+        equations: "EQUAZIONI DI CAMPO FONDAMENTALI",
+        from_intern:"MESSAGE FROM THE INTERN",
+        intern_msg:`Hello! I'm Sohn ✧｡٩(ˊᗜˋ )و✧*｡
+
+        I hope you enjoy exploring our project and gain some insights into optical arrangements and quantum fluid holography! ദ്ദി(ᵔᗜᵔ)
+    
+        Here is my LinkedIn ~`,
+        links_research: "Links & Research",
+        btn_uea: "UEA Webpage",
+        btn_pr: "Personal Research",
+        btn_ukri: "UKRI EPSRC Project",
+
+        pj_description:
+            `Quantum fluids are remarkable materials that behave according to the laws of quantum mechanics, allowing them to flow with almost no resistance and exhibit other unusual properties with great potential for future technologies such as quantum computing, ultra-precise sensing, and efficient heat transport. 
+            
+            This project aims to understand the most strongly interacting quantum fluids, whose behaviour cannot be explained using conventional theories. 
+            
+            To achieve this, we will use holographic duality - a powerful mathematical framework from particle physics and string theory—together with advanced numerical simulations to reveal new physics and guide future experiments`,
+            
+        thanks_text:"   (づ ᴗ _ᴗ)づ Special thanks to all the technicians for their help with the machines and tools (づ ᴗ _ᴗ)づ   "
+    },
+
+    it: {
+        top_title: `UEA Progetto "Ologramma"`, 
+        title: `Progetto "Ologramma"`,
+        faculty: "SCUOLA DI INGEGNERIA MATEMATICA E FISICA",
+        select_below: "SI PREGA DI SELEZIONARE",
+        btn_display: "Visualizza",
+        btn_guest: "Accesso Ospite",
+        btn_connect: "Connetti",
+        btn_proj_info: "Informazioni Progetto",
+        box_code: "Inserisci codice a 4 cifre",
+        guest_remote_code: "CODICE REMOTO OSPITE:",
+        guest_control: "Controllo Ospite",
+        cat_3d_objects: "MODELLI 3D",
+        cat_knots: "NODI E CURVE",
+        cat_geometry: "POLIEDRI E GEOMETRIA STANDARD",
+        cat_complex: "SIMULAZIONI AVANZATE",
+        adjustments: "REGOLAZIONI",
+        adj_size: "Regolazione della dimensione",
+        adj_side_x: "Oggetti laterali (asse x)",
+        adj_side_y: "Oggetti laterali (asse y)",
+        adj_mid_y: "Oggetto centrale (asse y)",
+        swipe_lr: "Scorri per ruotare a sinistra/destra",
+        swipe_obj: "Scorri per ruotare l'oggetto",
+        toggles: "INTERRUTTORI E TELECAMERA LIVE",
+        btn_color: "Cambia Colore",
+        color_title: "Colore",
+        btn_close_apply: "Chiudi / Applica",
+        btn_toggle_code: "Mostra/Nascondi Codice",
+        btn_toggle_lines: "Mostra/Nascondi Linee di Taglio",
+        btn_toggle_lang: "Cambia Lingua",
+        btn_live_cam: "Telecamera Live",
+        supervisor: "SUPERVISORE",
+        student_intern: "TIROCINANTE",
+        go_back: "Torna Indietro",
+        report: "Rapporto",
+        booklet: "Opuscolo",
+        flowchart: "Diagramma di flusso",
+        special_thanks: "Ringraziamenti Speciali:",
+        project_overview: "PANORAMICA DEL PROGETTO",
+        libraries_used: "LIBRERIE UTILIZZATE",
+        credits: "CREDITI",
+        dyn_temperature: "Temperatura",
+        dyn_wave: "Ampiezza dell'Onda",
+        dyn_evolution: "Tempo di Evoluzione",
+        eq_gp: "Equazione di Gross-Pitaevskii:",
+        eq_holographic: "Lagrangiana olografica materia-gauge-gravità:",
+        eq_navier: "Equazione di Navier-Stokes:",
+        equations: "EQUAZIONI DI CAMPO FONDAMENTALI",
+        from_intern:"MESSAGGIO DAL TIROCINANTE",
+        intern_msg:`Ciao! Sono Sohn ✧｡٩(ˊᗜˋ )و✧*｡
+        
+        Spero che ti piaccia esplorare il nostro progetto e che tu possa comprendere meglio le configurazioni ottiche e l'olografia dei fluidi quantistici! ദ്ദി(ᵔᗜᵔ)
+    
+        Ecco il mio LinkedIn ~`,
+        links_research: "Link e Ricerca",
+        btn_uea: "Pagina Web UEA",
+        btn_pr: "Ricerca Personale",
+        btn_ukri: "Progetto UKRI EPSRC",
+
+        eq_water_waves: "Onde d'acqua lineari sotto una sottile lastra di ghiaccio elastica:",
+        pj_description: 
+        `I fluidi quantistici sono materiali straordinari che si comportano secondo le leggi della meccanica quantistica, permettendo loro di scorrere quasi senza resistenza e di mostrare altre proprietà insolite con un grande potenziale per le tecnologie future, come il calcolo quantistico, la sensoristica ad altissima precisione e il trasporto efficiente del calore. 
+        
+        Questo progetto mira a comprendere i fluidi quantistici a interazione più forte, 
+        il cui comportamento non può essere spiegato utilizzando le teorie convenzionali. 
+        
+        Per raggiungere questo obiettivo, utilizzeremo la dualità olografica - un potente quadro matematico proveniente dalla fisica delle particelle e dalla teoria delle stringhe - 
+        insieme a simulazioni numeriche avanzate per svelare nuova fisica e guidare i futuri esperimenti.`,
+        
+        thanks_text:`   (づ ᴗ _ᴗ)づ Un ringraziamento speciale a tutti i tecnici per il loro aiuto con i macchinari e le attrezzature. (づ ᴗ _ᴗ)づ   `
+    }  
+};
+
+function Change_Language(lang) {
+    const elements = document.querySelectorAll('[data-lang]');
+    
+    elements.forEach((element) => {
+        const key = element.getAttribute('data-lang');
+        if (change_lang[lang] && change_lang[lang][key]) {
+            if (element.tagName === 'INPUT') {
+                element.placeholder = change_lang[lang][key];
+            } else {
+                element.innerText = change_lang[lang][key];
+            }
+        }
+    });
+    
+    localStorage.setItem('myAppLanguage', lang);
+    console.log("Language changed to: " + lang);
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    const savedLang = localStorage.getItem('myAppLanguage') || 'en';
+    Change_Language(savedLang);
+});
